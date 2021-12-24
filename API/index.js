6f0d83631fd2e9410ebe7c1f805cfa7be7c233be
septianwijaya45 <septianwijaya170845@gmail.com>
2021-12-24 08:14:43 +0700

    Verify JWT

--- API/index.js
+++ API/index.js
@@ -44,6 +44,24 @@ app.post("/api/login", (req, res) => {
   }
 });
 
+const verify = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, "mySecretKey", (err, user) => {
+      if (err) {
+        res.status(403).json("Token Isn't Valid!");
+      }
+
+      req.user = user;
+      next();
+    });
+  } else {
+    res.status(401).json("You Are Not Authenticated!");
+  }
+};
+
 app.listen(5000, () => {
   console.log(`Backend is running on port 5000`);
 });
